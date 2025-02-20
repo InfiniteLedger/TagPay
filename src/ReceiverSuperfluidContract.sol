b2573eab2c52bd5fcb8712821285bfaeab938d85
wfnuser <wfnuser@hotmail.com>
2025-02-20 20:49:16 +0800

    feat: support nested pool

--- src/ReceiverSuperfluidContract.sol
+++ src/ReceiverSuperfluidContract.sol
@@ -6,30 +6,55 @@ import "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Librar
 import {TagStream} from "./TagStream.sol";
 
 contract ReceiverSuperfluidContract {
-    using SuperTokenV1Library for ISuperToken; 
+    using SuperTokenV1Library for ISuperToken;
     ISuperToken public immutable acceptedToken;
     address public immutable tagStream;
     address public immutable superAdmin;
-    address public receiver; 
+    address public receiver;
 
-    constructor(address _acceptedToken, address _tagStream, address _superAdmin) {
+    constructor(
+        address _acceptedToken,
+        address _tagStream,
+        address _superAdmin
+    ) {
         acceptedToken = ISuperToken(_acceptedToken);
         tagStream = _tagStream;
         superAdmin = _superAdmin;
     }
 
     function setReceiver(address _receiver) external {
-        require(msg.sender == tagStream || msg.sender == superAdmin || msg.sender == receiver, "Only TagStream or SuperAdmin or Stream Receiver can set Receiver");
+        require(
+            msg.sender == tagStream ||
+                msg.sender == superAdmin ||
+                msg.sender == receiver,
+            "Only TagStream or SuperAdmin or Stream Receiver can set Receiver"
+        );
         receiver = _receiver;
     }
 
-    function connectToRepo(string memory repo) external {
+    function connectToRepo(string memory repo) public {
         acceptedToken.connectPool(TagStream(tagStream).repoPools(repo));
     }
 
     function claimRewards() external {
-        require(msg.sender == receiver, "Only Stream Receiver can claim rewards");
+        require(
+            msg.sender == receiver,
+            "Only Stream Receiver can claim rewards"
+        );
         uint256 amount = acceptedToken.balanceOf(address(this));
         acceptedToken.downgradeTo(msg.sender, amount);
     }
-} 
+
+    // flow from source repo to target repo
+    function flowToRepo(
+        string memory sourceRepo,
+        string memory targetRepo,
+        uint _amount,
+        uint _duration
+    ) external {
+        // connect to source repo
+        connectToRepo(sourceRepo);
+        // flow to target repo
+        TagStream(tagStream).flowToRepo(targetRepo, _amount, _duration);
+    }
+}
